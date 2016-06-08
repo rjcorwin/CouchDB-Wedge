50029d451ab9c6f099aadbe5fafe81e562d2cc4f
R.J. Steinert <rj@rjsteinert.com>
2016-06-08 18:35:05 -0400

    Add support for a file option on push-json

--- wedge-push-json.js
+++ wedge-push-json.js
@@ -2,39 +2,27 @@
 
 var program = require('commander');
 var pushJson = require('./lib/PushJson')
+var fs = require('fs')
 
 function puts(error, stdout, stderr) { sys.puts(stdout); sys.puts(stderr); sys.puts(error) } 
 
 program
   .option('-u, --url <url>', 'The URL of where to push documents to.', '')
+  .option('-f, --file <path>', 'Path to a file of JSON data.', '')
   .option('--verbose', 'Verbose mode')
 
 program.on('--help', function(){
   process.stdout.write('  Examples:')
   process.stdout.write('')
-  process.stdout.write('    $ wedge push-json --url http://username:password@foo.tangerinecentral.org/group-testsweet')
+  process.stdout.write('    $ wedge push-json --file data.json --url http://username:password@foo.tangerinecentral.org/group-testsweet')
   process.stdout.write('')
 });
 
 program.parse(process.argv);
 
-var stdin = process.stdin,
-    stdout = process.stdout,
-    inputChunks = [];
+if (program.file) {
 
-stdin.resume();
-stdin.setEncoding('utf8');
-
-stdin.on('data', function (chunk) {
-    inputChunks.push(chunk);
-});
-
-stdin.on('end', function () {
-
-  var inputJSON = inputChunks.join(),
-      parsedData = JSON.parse(inputJSON),
-      program.json = JSON.stringify(parsedData, null, '    ');
-
+  program.json = fs.readFileSync(program.file)
   pushJson(program, function(err, res) {
     if(err) {
       process.stderr.write(err)
@@ -47,5 +35,34 @@ stdin.on('end', function () {
     }   
   })
 
-});
+}
+else {
 
+  var stdin = process.stdin,
+      stdout = process.stdout,
+      inputChunks = [];
+
+  stdin.resume();
+  stdin.setEncoding('utf8');
+
+  stdin.on('data', function (chunk) {
+      inputChunks.push(chunk);
+  });
+
+  stdin.on('end', function () {
+    program.json = inputChunks.join()
+    pushJson(program, function(err, res) {
+      if(err) {
+        process.stderr.write(err)
+        process.stderr.write(res)
+        process.exit(1)
+      }
+      else {
+        process.stdout.write(res)
+        process.exit(0)
+      }   
+    })
+
+  });
+
+}
